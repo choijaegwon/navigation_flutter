d8c9d278ffd44f15a71373d0b8cf1d434d754f02
choijaegwon <chl9338@naver.com>
2023-06-06 15:14:24 +0900

    feat: 생명주기 테스트 추가

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 
-class Screen2 extends StatelessWidget {
+class Screen2 extends StatefulWidget {
+  @override
+  State<Screen2> createState() => _Screen2State();
+}
+
+class _Screen2State extends State<Screen2> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('initState Called');
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+    print('deactivate Called');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('BuildContext Called');
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
